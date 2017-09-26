a48ed9b481f7d84d5c4f18f21c500f63c95398ec
Arpit <banatiarpit97@yahoo.co.in>
2017-09-26 17:02:54 +0530

    print button and functionality added;can add a title before printing;stroke size and color change according to slider and color input;erase button to change to green color when selected;save functionality added-images saved in local storage; @

--- js/main.js
+++ js/main.js
@@ -4,15 +4,127 @@ var a = angular.module('DrawingApp', ['ngMaterial'])
             .primaryPalette('light-blue')
             .accentPalette('light-blue');
     });
-    a.controller('AppCtrl', function($scope) {
+    a.controller('AppCtrl', function($scope,  $mdDialog) {
 
-        $scope.width = 3;
+         $scope.title = function(ev) {
+                         $mdDialog.show({
+                                 controller: DialogController,
+                                 templateUrl: 'template/pageTitle.html',
+                                 parent: angular.element(document.body),
+                                 targetEvent: ev,
+                                 clickOutsideToClose:true// Only for -xs, -sm breakpoints.
+                         });
+
+                 };
+
+         function DialogController($scope, $mdDialog) {
+
+            $scope.rangeError =  function(){
+                window.alert('hh')
+            }
+                         $scope.close = function() {
+                                 $mdDialog.cancel();
+                         };
+                         $scope.cancel = function() {
+                                 $mdDialog.cancel();
+                         };
+                         $scope.confirmTile = function() {
+                                 $scope.rangeError();  
+                         }  
+        }                     
+        $scope.width = 2;
+        $scope.strokecolor;
         $("md-slider-container").click(function(){
             dimension = $scope.width + "px";
-            console.log(dimension);
             $(".stroke_size").css("width", dimension);
             $(".stroke_size").css("height", dimension);
+            ctx.lineWidth = $scope.width;
+
+        });
+
+        var paint = false;
+        var paintOrErase = "paint";
+        var canvas = document.getElementById("drawing");
+        var ctx = canvas.getContext("2d");
+        var canvasContainer = $(".canvas_container")
+
+        var mouse = {x:0, y:0};
+
+        //Get Saved Image
+        if(localStorage.getItem("prevImg") != null){
+            var img = new Image();
+            img.onload = function(){
+                ctx.drawImage(img, 0, 0);
+            }
+            img.src = localStorage.getItem("prevImg");
+        };
+        //Get Saved Image
+
+
+        ctx.lineWidth = 2;
+        ctx.lineJoin = "round";
+        ctx.lineCap = "round";
+
+        canvasContainer.mousedown(function(e){
+            paint = true;
+            ctx.beginPath();
+            mouse.x = e.pageX - this.offsetLeft;
+            mouse.y = e.pageY - this.offsetTop;
+            console.log(mouse.x)
+            ctx.moveTo(mouse.x, mouse.y);
+        });
+
+        canvasContainer.mousemove(function(e){
+            mouse.x = e.pageX - this.offsetLeft;
+            mouse.y = e.pageY - this.offsetTop; 
+           if(paint == true){
+                if(paintOrErase == "paint"){
+                    ctx.strokeStyle = $('.selectColor').val();
+                }
+                else{
+                    ctx.strokeStyle = "white"
+                }
+                ctx.lineTo(mouse.x, mouse.y);
+                ctx.stroke();
+            }
+        });
+        canvasContainer.mouseup(function(){
+            paint = false;
+        });
+        canvasContainer.mouseleave(function(){
+            paint = false;
+        });
+        $(".erase").click(function(){
+            if(paintOrErase == "paint"){
+                paintOrErase = "erase";
+                $(this).addClass('buttonSelected')
+            }else{
+                paintOrErase = "paint";
+                $(this).removeClass('buttonSelected')
+            }
 
         })
+        $(".clear").click(function(){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            paintOrErase = "paint";
+        })
+
+        $(".save").click(function(){
+            if(typeof(localStorage) != null){
+                localStorage.setItem("prevImg", canvas.toDataURL());
+            }
+            else{
+                window.alert('Your Web Browser does not support Local Storage');
+            }
+        });
+        
+        $('.selectColor').change(function(){
+            $('.stroke_size').css("background-color", $(this).val());
+            paintOrErase = "paint";
+            $(".erase").removeClass('buttonSelected')
+
+        });
 
     });
+
+
